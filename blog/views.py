bfd15fe5f71ccbe12bc9e22311ac2f87603cde62
Siddharth Roy <siddharthroy36912@gmail.com>
2021-01-09 14:28:04 +0530

    Added Paginator

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Blog
 
 def blogs_view(request):
+    blogs = Blog.objects.all()
+    paginator = Paginator(blogs, 5)
+    page_number = request.GET.get('page') if request.GET.get('page') else 1
+
+    page = paginator.get_page(page_number)
+
     context = {
         'page': 'blog',
-        'blogs': Blog.objects.all()
+        'page_num': page.start_index(),
+        'blogs': page,
+        'pages': paginator.num_pages,
+        'has_next': page.has_next(),
+        'has_prev': page.has_previous()
     }
 
     return render(request, 'blog/blogs.html', context)
